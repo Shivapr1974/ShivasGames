04b2ff6329fb0038a76ad83b45c013b0c248c271
Shiva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2018-08-20 13:30:27 -0400

    Initial Backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MathGamerComponent } from './math-gamer/math-gamer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mathGames', component: MathGamerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
