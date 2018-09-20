9d6b7ff7e33d8ed1b971bdb71edc3a0682dc9c2c
Shiva Ram <Sivakumar.1.Ramakrishnan@gm.com>
2018-09-20 14:04:55 -0400

    Shiva's Games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LevelComponent } from './level/level.component';
 import { MathgameshomeComponent } from './mathgameshome/mathgameshome.component';
+import { MathCalcsComponent } from './math-calcs/math-calcs.component';
+import { WordTriviaHomeComponent } from './word-trivia-home/word-trivia-home.component';
+import { WordTriviaGamesComponent } from './word-trivia-games/word-trivia-games.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path: '', component:HomeComponent},
   { path: 'mathHome', component: MathgameshomeComponent },
-  { path: 'level', component: LevelComponent }
-
+  { path: 'level/:type', component: LevelComponent },
+  { path: 'mathCalc/:type', component: MathCalcsComponent },
+  { path: 'wordTriviaHome/:category', component: WordTriviaHomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'wordTrivia/:category/:type', component: WordTriviaGamesComponent }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,36 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { MathGamesModule } from './math-games/math-games.module';
+import { LevelComponent } from './level/level.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WordTriviaModuleModule } from './word-trivia-module/word-trivia-module.module';
+import { SharedmoduleModule } from './sharedmodule/sharedmodule.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LevelComponent,
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    MathGamesModule 
+    MathGamesModule,
+    AngularFontAwesomeModule,
+    NgbModule,
+    WordTriviaModuleModule,
+    SharedmoduleModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [LevelComponent, NavbarComponent, AboutComponent, HomeComponent]
 })
 export class AppModule { }
